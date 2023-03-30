48db744b9cf6be33c1bcf9bd8398d0db03046048
303597 <124871430+303597@users.noreply.github.com>
2023-03-30 19:32:03 +0800

    Update characters.h

--- src/characters.h
+++ src/characters.h
@@ -8,7 +8,7 @@ class Character
 public:
     Character(int start_x = 0, int start_y = 0);
     void linkMap(Map* _map);
-    //int start_x, start_y; // initial position
+    int start_x, start_y; // initial position
     int x, y; // present position
     //bool in_counteratk_mode = false; // whether in counter-attack mode
 
