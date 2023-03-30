84275f14dd61ae6360bab002b771730c6deb059d
303597 <124871430+303597@users.noreply.github.com>
2023-03-31 02:53:22 +0800

    changed ghost::move function

--- src/characters.cpp
+++ src/characters.cpp
@@ -33,8 +33,9 @@ void PacMan::move(int direction)
 
 void Ghost::move(int target_x, int target_y, double speed)
 {
-    int co_x,co_y;//corresponding postions
+	int co_x,co_y;//corresponding postions
 	int multi = 1; // switch mode 
+	int fl=0, fx=1, fy=1;
 	int poss[4] = {1, 1, 1, 1}, cnt=4; // up down right left
 	int dirx[4] = {0, 0, 1, -1};
 	int diry[4] = {-1, 1, 0, 0};// up down right left
@@ -46,136 +47,182 @@ void Ghost::move(int target_x, int target_y, double speed)
 	}
 	srand(time(0));
 	int num = rand() % 12; //generate a random number range [0, 11]
-	for(int i = 0; i < (*ghosts).size(); i++){
-		for(int p = 0; p < 4; p++){
-			int xx = (*ghosts)[i].x + dirx[p];
-			int yy = (*ghosts)[i].y + diry[p];
-			if(xx < 0){ xx = map.map.vals.size() - 1; }
-			else if(xx > map.vals.size()){ xx = 0; }
-			if(yy < 0){ xx = map.vals[i].size() - 1; }
-			else if(yy > map.vals[i].size()){ xx = 0; }
-			// deal with overflow;
-			if(map.vals[xx][yy] == '#'){ poss[p] = 0; cnt--; }
-		}
-		// delete impossible moving directions; divide possibilities
-		co_x = (*ghosts)[i].x - target_x;
-    	co_y = (*ghosts)[i].y - target_y;
-    	// corresponding direction of the pacman to ghost
-    	if(co_x == 0){
-	    	if(co_y < 0 && poss[1] != 0){ (*ghosts)[i].y += diry[1]*speed; continue;}
-			else if (co_y > 0 && poss[0] != 0){ (*ghosts)[i].y += diry[0]*speed; continue;}
+	for(int p = 0; p < 4; p++){
+		int xx = x + dirx[p];
+		int yy = y + diry[p];
+		if(xx < 0){ xx = map.map.vals.size() - 1; }
+		else if(xx > map.vals.size()){ xx = 0; }
+		if(yy < 0){ xx = map.vals[x].size() - 1; }
+		else if(yy > map.vals[x].size()){ xx = 0; }
+		// deal with overflow;
+		if(map.vals[xx][yy] == '#'){ poss[p] = 0; cnt--; }
+	}
+	// delete impossible moving directions; divide possibilities
+	co_x = x - target_x;
+	co_y = y - target_y;
+	// corresponding direction of the pacman to ghost
+        if(!in_counteratk_mode){
+		if(co_x == 0){
+			if(map.vals[x][0] != '#' && map.vals[x][map.vals[x].size()] != '#'){
+				if(abs(co_y) > map.vals[x].size()/2){fy = -1;}
+			}// whether go through holes on the wall
+				if(co_y < 0 && poss[1] != 0){ y += fy*speed; fl=1; }
+				else if (co_y > 0 && poss[0] != 0){ y -= fy*speed; fl=1; }
 		}
 		if(co_y == 0){
-	    	if(co_x < 0 && poss[2] != 0){ (*ghosts)[i].x += dirx[2]*speed; continue;}
-			else if (co_x > 0 && poss[3] != 0){ (*ghosts)[i].x += dirx[3]*speed; continue;}
+			if(map.vals[0][y] != '#' && map.vals[map.vals.size()][y] != '#'){
+				if(abs(co_x) > map.vals.size()/2){fx = -1;}
+			}// whether go through holes on the wall
+			if(co_x < 0 && poss[2] != 0){ x += fx*speed; fl=1; }
+				else if (co_x > 0 && poss[3] != 0){ x -= fx*speed; fl=1; }
 		}
-		// if in line-- better track the pac-man.
-		int hori, verti, type; // up or down ; left or right
-		if( co_x < 0 ) { verti = 1; }
-		else if (co_x > 0 ){verti = 0;}
-		if(co_y < 0) {hori = 2;}
-		else if (co_y > 0){hori = 3;}
-		if(cnt == 1){type = 1;}
-		else if(cnt == 2){
-			if((poss[hori] == 0 && poss[verti] != 0) || (poss[hori] != 0 && poss[verti] == 0)){type = 3;}
-			else{type = 2;}
-		}
-		else if(cnt == 3){
-			if((poss[hori] == 0 && poss[verti] != 0) || (poss[hori] != 0 && poss[verti] == 0)){type = 4;}
-			else if(poss[hori] != 0 && poss[verti] != 0){type = 5;}
-		}
-		else{type = 6;}
-		// 12:0 6:6 10:2 8:2:2 5:5:2 5:5:1:1			
-		switch (type)
-		{
-			case 1:
-				for(int p = 0; p < 4; p++){
-					if(poss[i] != 0){(*ghosts)[i].x += dirx[p]*speed; (*ghosts)[i].y += diry[p]*speed; break; }
-				}
-				break;
-			case 2:
-				for(int p = 0; p < 4; p++){
-					if(poss[i] != 0){
-						if(num % 2 == 0){(*ghosts)[i].x += dirx[p]*speed; (*ghosts)[i].y += diry[p]*speed; break; }
-					}
-					else{
-						num += 1; 
-						continue;
-					}
-				}
-				break;
-			case 3:
-				if(poss[hori] == 0 && poss[verti] != 0){
-					if(num <= 9 && num >= 0){
-						(*ghosts)[i].x += dirx[verti]*speed; (*ghosts)[i].y += diry[verti]*speed;	
-					}
-					else{
-						(*ghosts)[i].x += dirx[5-hori]*speed; (*ghosts)[i].y += diry[5-hori]*speed;
-					}
-				}
-				else{
-					if(num <= 9 && num >= 0){
-						(*ghosts)[i].x += dirx[hori]*speed; (*ghosts)[i].y += diry[hori]*speed;
-					}
-					else{
-						(*ghosts)[i].x += dirx[3-verti]*speed; (*ghosts)[i].y += diry[3-verti]*speed;
-					}
-				}
-				break;
-			case 5:
-				if(num <= 4 && num >= 0) {
-					(*ghosts)[i].x += dirx[verti]*speed; (*ghosts)[i].y += diry[verti]*speed; 
-				}
-				else if(num <=9 && num >-5){
-					(*ghosts)[i].x += dirx[hori]*speed; (*ghosts)[i].y += diry[hori]*speed; 
-				}
-				else{
-					if(poss[5-hori] != 0){
-						(*ghosts)[i].x += dirx[5-hori]*speed; (*ghosts)[i].y += diry[5-hori]*speed;
-					}
-					else{
-						(*ghosts)[i].x += dirx[3-verti]*speed; (*ghosts)[i].y += diry[3-verti]*speed;
-					}
-				}
-				break;
-			case 4:
-				if(num >= 0 && num <= 7){
-					if(poss[hori] == 0 && poss[verti] != 0){
-						(*ghosts)[i].x += dirx[verti]*speed; (*ghosts)[i].y += diry[verti]*speed;
-					}
-					else{
-						(*ghosts)[i].x += dirx[hori]*speed; (*ghosts)[i].y += diry[hori]*speed;
-					}
-				}
-				else if(num >= 8 && num <=9){
-					(*ghosts)[i].x += dirx[5-hori]*speed; (*ghosts)[i].y += diry[5-hori]*speed;
-				}
-				else{
-					(*ghosts)[i].x += dirx[3-verti]*speed; (*ghosts)[i].y += diry[3-verti]*speed;
-				}
-				break;
-			case 6:
-				if(num <= 4 && num >= 0){
-					(*ghosts)[i].x += dirx[verti]*speed; (*ghosts)[i].y += diry[verti]*speed;
-				}
-				else if(num <= 9 && num >= 5){
-					(*ghosts)[i].x += dirx[hori]*speed; (*ghosts)[i].y += diry[hori]*speed; 
-				}
-				else if(num == 10){
-					(*ghosts)[i].x += dirx[5-hori]*speed; (*ghosts)[i].y += diry[5-hori]*speed;
-				}
-				else{
-					(*ghosts)[i].x += dirx[3-verti]*speed; (*ghosts)[i].y += diry[3-verti]*speed;
-				}
-				break;		
-		}
-		// if in line-- better avoid the pac-man.
-		if((*ghosts)[i].x < 0){ (*ghosts)[i].x += map.vals.size(); }
-		else if((*ghosts)[i].x > map.vals.size()){ (*ghosts)[i].x -= map.vals.size(); }
-		if((*ghosts)[i].y < 0){ (*ghosts)[i].y += map.vals[i].size(); }
-		else if((*ghosts)[i].y > map.vals[i].size()){ (*ghosts)[i].y -= map.vals.size(); }
-		// deal with overflow;
 	}
+	else{
+		if(co_x == 0){
+	   		if(poss[2] != 0 && poss[3] != 0){
+	   			if(num % 2 == 0){x += speed;}
+	   			else {x -= speed;}
+	   			fl=1;
+			}
+			else if(poss[2] != 0 && poss[3] == 0){
+				x += speed;
+				fl=1;
+			}
+			else if(poss[2] == 0 && poss[3] != 0){
+				x -= speed;
+				fl=1;
+			}
+		}
+		if(co_y == 0){
+	    		if(poss[0] != 0 && poss[1] != 0){
+	   			if(num % 2 == 0){y += speed;}
+	   			else {y -= speed;}
+	   			fl=1;
+			}
+			else if(poss[0] != 0 && poss[1] == 0){
+				y += speed;
+				fl=1;
+			}
+			else if(poss[0] == 0 && poss[1] != 0){
+				y -= speed;
+				fl=1;
+			}
+		}
+	}
+	if (fl == 1){
+		if(x < 0){ x += map.vals.size(); }
+		else if(x > map.vals.size()){ x -= map.vals.size(); }
+		if(y < 0){ y += map.vals[x].size(); }
+		else if(y > map.vals[x].size()){ y -= map.vals.size(); }
+		// deal with overflow;
+		return;
+	}
+	// if in the same line-- better track or avoid the pac-man.
+	int hori, verti, type; // up or down ; left or right
+	if( co_x < 0 ) { verti = 1; }
+	else if (co_x > 0 ){verti = 0;}
+	if(co_y < 0) {hori = 2;}
+	else if (co_y > 0){hori = 3;}
+	if(cnt == 1){type = 1;}
+	else if(cnt == 2){
+		if((poss[hori] == 0 && poss[verti] != 0) || (poss[hori] != 0 && poss[verti] == 0)){type = 3;}
+		else{type = 2;}
+	}
+	else if(cnt == 3){
+		if((poss[hori] == 0 && poss[verti] != 0) || (poss[hori] != 0 && poss[verti] == 0)){type = 4;}
+		else if(poss[hori] != 0 && poss[verti] != 0){type = 5;}
+	}
+	else{type = 6;}
+	// 12:0 6:6 10:2 8:2:2 5:5:2 5:5:1:1			
+	switch (type)
+	{
+		case 1:
+			for(int p = 0; p < 4; p++){
+				if(poss[i] != 0){x += dirx[p]*speed; y += diry[p]*speed; break; }
+			}
+			break;
+		case 2:
+			for(int p = 0; p < 4; p++){
+				if(poss[i] != 0){
+					if(num % 2 == 0){x += dirx[p]*speed; y += diry[p]*speed; break; }
+				}
+				else{
+					num += 1; 
+					continue;
+				}
+			}
+			break;
+		case 3:
+			if(poss[hori] == 0 && poss[verti] != 0){
+				if(num <= 9 && num >= 0){
+					x += dirx[verti]*speed; y += diry[verti]*speed;	
+				}
+				else{
+					x += dirx[5-hori]*speed; y += diry[5-hori]*speed;
+				}
+			}
+			else{
+				if(num <= 9 && num >= 0){
+					x += dirx[hori]*speed; y += diry[hori]*speed;
+				}
+				else{
+					x += dirx[3-verti]*speed; y += diry[3-verti]*speed;
+				}
+			}
+			break;
+		case 5:
+			if(num <= 4 && num >= 0) {
+				x += dirx[verti]*speed; y += diry[verti]*speed; 
+			}
+			else if(num <=9 && num >-5){
+				x += dirx[hori]*speed; y += diry[hori]*speed; 
+			}
+			else{
+			if(poss[5-hori] != 0){
+					x += dirx[5-hori]*speed; y += diry[5-hori]*speed;
+				}
+				else{
+					x += dirx[3-verti]*speed; y += diry[3-verti]*speed;
+				}
+			}
+			break;
+		case 4:
+			if(num >= 0 && num <= 7){
+				if(poss[hori] == 0 && poss[verti] != 0){
+					x += dirx[verti]*speed; y += diry[verti]*speed;
+				}
+				else{
+					x += dirx[hori]*speed; y += diry[hori]*speed;
+				}
+			}
+			else if(num >= 8 && num <=9){
+				x += dirx[5-hori]*speed; y += diry[5-hori]*speed;
+			}
+			else{
+				x += dirx[3-verti]*speed; y += diry[3-verti]*speed;
+			}
+			break;
+		case 6:
+			if(num <= 4 && num >= 0){
+				x += dirx[verti]*speed; y += diry[verti]*speed;
+			}
+			else if(num <= 9 && num >= 5){
+				x += dirx[hori]*speed; y += diry[hori]*speed; 
+			}
+			else if(num == 10){
+				x += dirx[5-hori]*speed; y += diry[5-hori]*speed;
+			}
+			else{
+				x += dirx[3-verti]*speed; y += diry[3-verti]*speed;
+			}
+			break;		
+	}
+	// if in line-- better avoid the pac-man.
+	if(x < 0){ x += map.vals.size(); }
+	else if(x > map.vals.size()){ x -= map.vals.size(); }
+	if(y < 0){ y += map.vals[x].size(); }
+	else if(y > map.vals[x].size()){ y -= map.vals.size(); }
+	// deal with overflow;
     return;
 }
 
