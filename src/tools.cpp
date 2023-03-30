9cc5984ba2441f1887d9cb08bd26e78fe96b1de2
303597 <124871430+303597@users.noreply.github.com>
2023-03-30 20:12:19 +0800

    add different mode on printMapElement ghost

--- src/tools.cpp
+++ src/tools.cpp
@@ -79,7 +79,10 @@ void Map::printMapElement(int x, int y, char element)
             mvprintw(x, y, "◽");
             break;
         case 'E':
-            mvprintw(x, y, "👻");
+            if(in_counteratk_mode){
+                mvprintw(x, y, "👻");}
+            else{
+                mvprintw(x, y, "🥶");}
             break;
         default:
             mvprintw(x, y, "ㅤ");
