304e92ad4a927c6393a159e2030afdc1c60db296
303597 <124871430+303597@users.noreply.github.com>
2023-03-31 17:41:01 +0800

    add initializer

--- src/characters.cpp
+++ src/characters.cpp
@@ -37,11 +37,18 @@ void PacMan::move(int direction)
 
 void Ghost::move(int target_x, int target_y, double speed)
 {
-	//x = 7;
-	//y = 10;
+	int f=0;
+	if(x == start_x && start_y == y){
+		if(map->vals[x-1][y] == '_'){
+			x = x - 2; y = y;
+		}
+		else{
+			x = x + 2; y = y;
+		}
+	}//initializing
 	int co_x, co_y; // corresponding postions
 	int fl = 0, fx = 1, fy = 1;
-	int poss[4] = {1, 1, 1, 1}, cnt = 4; // up down left right
+	int poss[4] = {1, 1, 1, 1}; // up down left right
 	int dirx[4] = {-1, 1, 0, 0};
 	int diry[4] = {0, 0, -1, 1}; // up down left right
 	if (in_counteratk_mode)
@@ -52,8 +59,6 @@ void Ghost::move(int target_x, int target_y, double speed)
 			diry[i] *= -1;
 		}
 	}
-	srand(time(0));
-	int num = rand() % 12; // generate a random number range [0, 11]
 	for (int p = 0; p < 4; p++)
 	{
 		int xx = x + dirx[p];
@@ -75,19 +80,18 @@ void Ghost::move(int target_x, int target_y, double speed)
 			yy = 0;
 		}
 		// deal with overflow;
-		if (map->vals[xx][yy] == '#')
+		if (map->vals[xx][yy] == '#' || map->vals[xx][yy] == '_')
 		{
 			poss[p] = 0;
-			cnt--;
 		}
 	}
 	// delete impossible moving directions;
 	co_x = x - target_x;
 	co_y = y - target_y;
 	// corresponding direction of the pacman to ghost
-	if (!in_counteratk_mode)
+	/*if (!in_counteratk_mode)
 	{
-		if (co_x == 0)
+		if (co_y == 0)
 		{
 			if (map->vals[x][0] != '#' && map->vals[x][map->vals[x].size()-1] != '#')
 			{
@@ -96,15 +100,29 @@ void Ghost::move(int target_x, int target_y, double speed)
 					fy = -1;
 				}
 			} // whether go through holes on the wall
-			if (co_y < 0 && poss[1] != 0)
+			if (poss[1] != 0)
 			{
-				y += fy;
-				fl = 1;
+				if(co_y < 0 && fy == 1)
+				{	y += 1;
+					fl = 1;
+				}
+				else if(co_y > 0 && fy == -1)
+				{
+					y -= 1;
+					fl = 1;
+				}
 			}
-			else if (co_y > 0 && poss[0] != 0)
+			if (poss[0] != 0)
 			{
-				y -= fy;
-				fl = 1;
+				if(co_y > 0 && fy == 1)
+				{	y += 1;
+					fl = 1;
+				}
+				else if(co_y < 0 && fy == -1)
+				{
+					y -= 1;
+					fl = 1;
+				}
 			}
 		}
 		if (co_y == 0)
@@ -116,25 +134,41 @@ void Ghost::move(int target_x, int target_y, double speed)
 					fx = -1;
 				}
 			} // whether go through holes on the wall
-			if (co_x < 0 && poss[3] != 0)
+			if (poss[2] != 0)
 			{
-				x += fx;
-				fl = 1;
+				if(co_x < 0 && fy == 1)
+				{	y += 1;
+					fl = 1;
+				}
+				else if(co_y > 0 && fy == -1)
+				{
+					y -= 1;
+					fl = 1;
+				}
 			}
-			else if (co_x > 0 && poss[2] != 0)
+			if (poss[3] != 0)
 			{
-				x -= fx;
-				fl = 1;
+				if(co_y > 0 && fy == 1)
+				{	y += 1;
+					fl = 1;
+				}
+				else if(co_y < 0 && fy == -1)
+				{
+					y -= 1;
+					fl = 1;
+				}
 			}
 		}
 	}
 	else
 	{
+		srand(time(0));
+		int number = rand();
 		if (co_x == 0)
 		{
 			if (poss[2] != 0 && poss[3] != 0)
 			{
-				if (num % 2 == 0)
+				if (number % 2 == 0)
 				{
 					x += 1;
 				}
@@ -155,11 +189,11 @@ void Ghost::move(int target_x, int target_y, double speed)
 				fl = 1;
 			}
 		}
-		if (co_y == 0)
+		else if (co_y == 0)
 		{
 			if (poss[0] != 0 && poss[1] != 0)
 			{
-				if (num % 2 == 0)
+				if (number % 2 == 0)
 				{
 					y += 1;
 				}
@@ -201,7 +235,7 @@ void Ghost::move(int target_x, int target_y, double speed)
 		}
 		// deal with overflow;
 		return;
-	}
+	}*/
 	// if in the same line-- better track or avoid the pac-man.
 	int hori, verti, type; // up or down ; left or right
 	if (co_x <= 0)
@@ -220,171 +254,27 @@ void Ghost::move(int target_x, int target_y, double speed)
 	{
 		hori = 2;
 	}
-	if (cnt == 1)
-	{
-		type = 1;
-	}
-	else if (cnt == 2)
-	{
-		if ((poss[hori] == 0 && poss[verti] != 0) || (poss[hori] != 0 && poss[verti] == 0))
-		{
-			type = 3;
+	int sum=0;
+	for(int p = 0; p < 4; p++){
+		if(poss[p] == 0){
+			continue;
 		}
-		else
-		{
-			type = 2;
+		if(hori == p || verti == p){
+			poss[p] = 5 + sum; sum += 5;
+		}
+		else{
+			poss[p] = 1 + sum; sum += 1;
 		}
 	}
-	else if (cnt == 3)
-	{
-		if ((poss[hori] == 0 && poss[verti] != 0) || (poss[hori] != 0 && poss[verti] == 0))
-		{
-			type = 4;
-		}
-		else if (poss[hori] != 0 && poss[verti] != 0)
-		{
-			type = 5;
+	srand(time(0));
+	int num = rand() % sum; // generate a random number range [0, sum-1]
+	for(int p = 0; p < 4; p++){
+		if(num <= poss[p]){
+			x += dirx[p];
+			y += diry[p];
+			break;
 		}
 	}
-	else
-	{
-		type = 6;
-	}
-	// 12:0 6:6 10:2 8:2:2 5:5:2 5:5:1:1
-	switch (type)
-	{
-	case 1:
-		for (int p = 0; p < 4; p++)
-		{
-			if (poss[p] != 0)
-			{
-				x += dirx[p];// * speed;
-				y += diry[p];// * speed;
-				break;
-			}
-		}
-		break;
-	case 2:
-		for (int p = 0; p < 4; p++)
-		{
-			if (poss[p] != 0)
-			{
-				if (num % 2 == 0)
-				{
-					x += dirx[p];// * speed;
-					y += diry[p];// * speed;
-					break;
-				}
-			}
-			else
-			{
-				num += 1;
-				continue;
-			}
-		}
-		break;
-	case 3:
-		if (poss[hori] == 0 && poss[verti] != 0)
-		{
-			if (num <= 9 && num >= 0)
-			{
-				x += dirx[verti];// * speed;
-				y += diry[verti];// * speed;
-			}
-			else
-			{
-				x += dirx[5 - hori];// * speed;
-				y += diry[5 - hori];// * speed;
-			}
-		}
-		else
-		{
-			if (num <= 9 && num >= 0)
-			{
-				x += dirx[hori];// * speed;
-				y += diry[hori];// * speed;
-			}
-			else
-			{
-				x += dirx[1 - verti];// * speed;
-				y += diry[1 - verti];// * speed;
-			}
-		}
-		break;
-	case 5:
-		if (num <= 4 && num >= 0)
-		{
-			x += dirx[verti];// * speed;
-			y += diry[verti];// * speed;
-		}
-		else if (num <= 9 && num > -5)
-		{
-			x += dirx[hori];// * speed;
-			y += diry[hori];// * speed;
-		}
-		else
-		{
-			if (poss[5 - hori] != 0)
-			{
-				x += dirx[5 - hori];// * speed;
-				y += diry[5 - hori];// * speed;
-			}
-			else
-			{
-				x += dirx[1 - verti];// * speed;
-				y += diry[1 - verti];// * speed;
-			}
-		}
-		break;
-	case 4:
-		if (num >= 0 && num <= 7)
-		{
-			if (poss[hori] == 0 && poss[verti] != 0)
-			{
-				x += dirx[verti];// * speed;
-				y += diry[verti];// * speed;
-			}
-			else
-			{
-				x += dirx[hori];// * speed;
-				y += diry[hori];// * speed;
-			}
-		}
-		else if (num >= 8 && num <= 9)
-		{
-			x += dirx[5 - hori];// * speed;
-			y += diry[5 - hori];// * speed;
-		}
-		else
-		{
-			x += dirx[1 - verti];// * speed;
-			y += diry[1 - verti];// * speed;
-		}
-		break;
-	case 6:
-		if (num <= 4 && num >= 0)
-		{
-			x += dirx[verti];// * speed;
-			y += diry[verti];// * speed;
-		}
-		else if (num <= 9 && num >= 5)
-		{
-			x += dirx[hori];// * speed;
-			y += diry[hori];// * speed;
-		}
-		else if (num == 10)
-		{
-			x += dirx[5 - hori];// * speed;
-			y += diry[5 - hori];// * speed;
-		}
-		else
-		{
-			x += dirx[1 - verti];// * speed;
-			y += diry[1 - verti];// * speed;
-		}
-		break;
-	}
-	// if in line-- better avoid the pac-man.
 	if (x < 0)
 	{
 		x += map->vals.size();
@@ -402,7 +292,6 @@ void Ghost::move(int target_x, int target_y, double speed)
 		y -= map->vals[x].size();
 	}
 	// deal with overflow;
-	
 	return;
 }
 
