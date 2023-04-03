d5101d079369877a8cc524625d1902898d38017e
Yangliuqing09 <willow33@connect.hku.hk>
2023-04-03 16:30:26 +0800

    declare score variable and save function

--- src/game.h
+++ src/game.h
@@ -3,6 +3,10 @@
 
 const double time_per_loop = 400; // milliseconds
 
+extern int score; // declare a score variable that can be modified in other files
+
+void save(); // declare the save function
+
 void welcomeLoop();
 
 bool gameLoop();
