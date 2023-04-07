b75af86b40e841dc150713401321189bfee370a1
Yangliuqing09 <willow33@connect.hku.hk>
2023-04-07 15:34:05 +0800

    declare readFromFile function

--- src/tools.h
+++ src/tools.h
@@ -31,6 +31,7 @@ public:
     bool isWall(int x, int y);
     int updateTile(int x, int y); // return 0: no buff, 1: energizer
     void saveToFile(string filename);
+    void readFromFile(string filename, PacMan &_pacman, vector<Ghost> &_ghosts);
     vector<string> vals;
 };
 
