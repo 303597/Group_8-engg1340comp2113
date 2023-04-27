e72e8f593cff21141e4709bc64f692c9093288b7
Yihan Liu <112919070+YihanLiu000@users.noreply.github.com>
2023-04-27 22:06:21 +0800

    add isMapCompleted()
    update initializeGame() & isMapCompleted() & gameLoop

--- src/game.h
+++ src/game.h
@@ -1,6 +1,9 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <string>
+using namespace std;
+
 const double time_per_loop = 400; // milliseconds
 
 extern int score; // declare a score variable that can be modified in other files
@@ -8,6 +11,10 @@ extern int score; // declare a score variable that can be modified in other file
 void save(); // declare the save function
 
 int welcomeLoop();
+
+void initializeGame();
+
+bool isMapCompleted(const string& filename);
 
 bool gameLoop(int level);
 
