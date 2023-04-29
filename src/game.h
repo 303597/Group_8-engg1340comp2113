0b8b87f1a87c1612a34135172ccf11b62474ba1c
Yangliuqing09 <willow33@connect.hku.hk>
2023-04-29 15:48:00 +0800

    modify class Game

--- src/game.h
+++ src/game.h
@@ -8,6 +8,15 @@ const double time_per_loop = 400; // milliseconds
 
 extern int score; // declare a score variable that can be modified in other files
 
+template <typename T> T read_line(string line)
+{
+    istringstream record(line);
+    string name;
+    T value;
+    record >> name >> value;
+    return value;
+}
+
 class Game
 {
 private:
@@ -15,13 +24,21 @@ private:
     Map* game_map;
     PacMan* pacman; vector<Ghost>* ghosts;
     Menu* game_menu;
+    
     bool in_counteratk_mode = false;
     int turns = 0;
     double ghost_speed = 0.4;
+    
     int updateTile(int x, int y, string &special, int &prop_turns);
     void showStatus();
     void saveToFile(string filename);
     void checkCharacterCollision();
+    
+    int prop_type = -1, prop_turns = 0, prop_lasting_time = 0;
+    int fruit_num = -1, fruit_lasting_time = 0;
+    int prop_pos_x = game_map->vals.size() - 1, prop_pos_y = game_map->vals[prop_pos_x].size() - 1;
+    int fruit_pos_x = game_map->vals.size() - 1, fruit_pos_y = game_map->vals[fruit_pos_x].size() - 1;
+    
 public:
     Game(int level);
     Game(string filename);
